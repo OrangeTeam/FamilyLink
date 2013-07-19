16d64694660e51f9ebb8058249b57484ba08a070
Bai-Jie <BaiJie1991@gmail.com>
2013-07-19 21:14:02 +0800

    使用ActionBarSherlock
    修改 主题设置和Activity，以使ActionBarSherlock生效

--- app/src/org/orange/familylink/MainActivity.java
+++ app/src/org/orange/familylink/MainActivity.java
@@ -1,10 +1,12 @@
 package org.orange.familylink;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+
+
+public class MainActivity extends SherlockFragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +17,7 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getSupportMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
