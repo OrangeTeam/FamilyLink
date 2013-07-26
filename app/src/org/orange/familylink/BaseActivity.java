122e7cfbb2fbfe391952a8a21b67217b8da6a2c1
Bai-Jie <BaiJie1991@gmail.com>
2013-07-26 11:38:26 +0800

    利用Android ActionBar的新特性简化代码
    修改 利用系统提供的导航功能，简化导航代码
    修改 menu的一些属性的命名空间

--- app/src/org/orange/familylink/BaseActivity.java
+++ app/src/org/orange/familylink/BaseActivity.java
@@ -3,51 +3,18 @@
  */
 package org.orange.familylink;
 
-import android.content.Intent;
-import android.support.v4.app.NavUtils;
-import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-import android.view.MenuItem;
 
 /**
  * @author Team Orange
  */
-public class BaseActivity extends ActionBarActivity {
+public abstract class BaseActivity extends ActionBarActivity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			// and how to implement it:
-			// http://developer.android.com/intl/zh-cn/training/implementing-navigation/ancestral.html#BuildBackStack
-			Intent upIntent = NavUtils.getParentActivityIntent(this);
-			if (NavUtils.shouldUpRecreateTask(this, upIntent)) {
-				// This activity is NOT part of this app's task, so create a new task
-				// when navigating up, with a synthesized back stack.
-				TaskStackBuilder.create(this)
-						// Add all of this activity's parents to the back stack
-						.addNextIntentWithParentStack(upIntent)
-						// Navigate up to the closest parent
-						.startActivities();
-			} else {
-				// This activity is part of this app's task, so simply
-				// navigate up to the logical parent activity.
-				NavUtils.navigateUpTo(this, upIntent);
-			}
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 }
