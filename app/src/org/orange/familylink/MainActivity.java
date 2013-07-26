9779f4b9b14687b60051241218afd82df7d5cf76
Bai-Jie <BaiJie1991@gmail.com>
2013-07-26 16:52:45 +0800

    优化MainActivity的layout结构
    新增 引入Split Action Bar
    修改 横屏时把Tabs折叠到主Action Bar中
    修改 竖屏时求助Page使用左右结构

--- app/src/org/orange/familylink/MainActivity.java
+++ app/src/org/orange/familylink/MainActivity.java
@@ -4,6 +4,7 @@ import org.orange.familylink.fragment.LogFragment;
 import org.orange.familylink.fragment.NavigateFragment;
 import org.orange.familylink.fragment.SeekHelpFragment;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -37,6 +38,12 @@ public class MainActivity extends BaseActivity {
 		ActionBar actionBar = getSupportActionBar();
 		// Specify that the Home/Up button should not be enabled, since there is no hierarchical parent.
 		actionBar.setHomeButtonEnabled(false);
+		if(getResources().getConfiguration().orientation ==
+				Configuration.ORIENTATION_PORTRAIT) {
+			//Let navigation tabs to collapse into the main action bar
+			actionBar.setDisplayShowHomeEnabled(false);
+			actionBar.setDisplayShowTitleEnabled(false);
+		}
 		// Specify that tabs should be displayed in the action bar.
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 		// Create a tab listener that is called when the user changes tabs.
