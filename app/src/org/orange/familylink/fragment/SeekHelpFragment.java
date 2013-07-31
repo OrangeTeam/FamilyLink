8a832752a02619fbf70b5ff68c6c1f2fdd26c28b
Bai-Jie <BaiJie1991@gmail.com>
2013-07-31 20:09:24 +0800

    去除多余的联系人Activity和求助子Fragments

--- app/src/org/orange/familylink/fragment/SeekHelpFragment.java
+++ app/src/org/orange/familylink/fragment/SeekHelpFragment.java
@@ -3,15 +3,12 @@
  */
 package org.orange.familylink.fragment;
 
-import org.orange.familylink.ListContactsActivity;
 import org.orange.familylink.R;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 
 /**
@@ -24,18 +21,7 @@ public class SeekHelpFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_seek_help, container, false);
-		rootView.findViewById(R.id.contact).setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				listContacts();
-			}
-		});
 		return rootView;
-	}
-
-	protected void listContacts(){
-		Intent intent = new Intent(getActivity(), ListContactsActivity.class);
-		startActivity(intent);
 	}
 
 }
