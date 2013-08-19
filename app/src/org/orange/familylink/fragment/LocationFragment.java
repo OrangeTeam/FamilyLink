bba974b274158b532683c49f893f5ea132d0b5f8
Jeffrey <jeffrey.nam@foxmail.com>
2013-08-19 10:54:38 +0800

    Revert "加入摔倒警报的倒计时Activity"
    This reverts commit 7393fd45c5bb5c1e3e486407d8ec964599a71a11.

--- app/src/org/orange/familylink/fragment/LocationFragment.java
+++ app/src/org/orange/familylink/fragment/LocationFragment.java
@@ -37,11 +37,7 @@ public class LocationFragment extends Fragment {
 		LocationInfo locationInfo = new LocationInfo(locationManager, 1000);
 		Position po =locationInfo.getCurrentLocationInfo() ;
 		//通过locationInfo的getCurrentLocationInfo方法获得当前位置信息，返回Postion对象，转换为String输出
-		if(po != null){
-			text.setText(po.toString());
-		}else{
-			text.setText("");
-		}
+		text.setText(po.toString());
 		return view;
 	}
 
