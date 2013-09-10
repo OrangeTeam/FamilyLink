e25a8d7ce94347eab020ee809eacc9c9a8d1ff49
Bai-Jie <BaiJie1991@gmail.com>
2013-09-10 17:12:58 +0800

    主Activity改为展示功能图标
    修改 主Activity不再使用Tab导航，改为显示功能图标列表

--- app/src/org/orange/familylink/MainActivity.java
+++ app/src/org/orange/familylink/MainActivity.java
@@ -1,27 +1,30 @@
 package org.orange.familylink;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.orange.familylink.R.drawable;
+import org.orange.familylink.R.string;
 import org.orange.familylink.data.Settings;
 import org.orange.familylink.data.Settings.Role;
 import org.orange.familylink.database.Contract;
 import org.orange.familylink.fragment.LogFragment;
-import org.orange.familylink.fragment.NavigateFragment;
-import org.orange.familylink.fragment.SeekHelpFragment;
 import org.orange.familylink.fragment.dialog.InitialSetupDialogFragment;
 
 import android.app.ActionBar;
-import android.app.ActionBar.Tab;
 import android.app.Activity;
 import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v13.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 
 /**
  * 主{@link Activity}。应用的默认{@link Activity}
@@ -54,17 +57,9 @@ public class MainActivity extends BaseActivity {
 	public static final String EXTRA_CONTACT_ID =
 			MainActivity.class.getName() + ".extra.CONTACT_ID";
 
-	// 用string ID表示页面及其顺序
-	/** 照料者的页面及其顺序 */
-	private static final int[] PAGERS_ORDER_CARER =
-			new int[]{R.string.log, R.string.seek_help, R.string.navigate};
-	/** 受顾者的页面及其顺序 */
-	private static final int[] PAGERS_ORDER_CAREE =
-			new int[]{R.string.seek_help, R.string.log, R.string.navigate};
-
-	private ViewPager mViewPager;
+	private GridView mMainMenuGridView;
 	private Role mRole;
-	private int[] mPagersOrder;
+	private Function[] mFunctions;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -75,7 +70,7 @@ public class MainActivity extends BaseActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		setProgressBarIndeterminateVisibility(false);
-		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mMainMenuGridView = (GridView) findViewById(R.id.main_menu);
 
 		setup();
 		handleIntent();
@@ -84,28 +79,28 @@ public class MainActivity extends BaseActivity {
 	@Override
 	protected void onStart() {
 		super.onStart();
-		changePagersOrderIfNecessary();
+		changeMainMenuIfNecessary();
 	}
 
 	/**
-	 * 根据用户角色，更改Pagers的顺序配置。
+	 * 根据用户角色，设置主菜单内容。
 	 * @param role 当前用户角色
 	 */
-	protected void setPagersOrder(Role role) {
-		if(role == Role.CARER)
-			mPagersOrder = PAGERS_ORDER_CARER;
-		else if(role == Role.CAREE)
-			mPagersOrder = PAGERS_ORDER_CAREE;
+	protected void setMainMenuContent(Role role) {
+		if(role == Role.CAREE)
+			mFunctions = FUNCTIONS_CAREE;
+		else if(role == Role.CARER)
+			mFunctions = FUNCTIONS_CARER;
 		else if(role == null) {
 			// 还没有配置用户角色， 现在配置
-			mPagersOrder = PAGERS_ORDER_CAREE;	// 设置默认的临时页面顺序
+			mFunctions = FUNCTIONS_GENERAL;
 			// 弹出对话框
 			InitialSetupDialogFragment dialog = new InitialSetupDialogFragment();
 			dialog.setOnClickListener(new InitialSetupDialogFragment.OnClickListener() {
 				@Override
 				public void onClickPositiveButton(InitialSetupDialogFragment dialog,
 						Role newRole, String newPassword) {
-					changePagersOrderIfNecessary();
+					changeMainMenuIfNecessary();
 				}
 			});
 			dialog.show(getFragmentManager(), null);
@@ -114,25 +109,13 @@ public class MainActivity extends BaseActivity {
 			throw new IllegalArgumentException("ilegal role: " + role);
 		mRole = role;
 	}
-	/**
-	 * 取得指定pager的位置
-	 * @param pagerId 要查找位置的Pager的Id。用R.string.*表示
-	 * @return 指定Pager的位置；如果无指定Pager的位置，返回-1
-	 * @see #mPagersOrder
-	 */
-	protected int getPagerPosition(int pagerId) {
-		for(int position = 0 ; position < mPagersOrder.length ; position++)
-			if(mPagersOrder[position] == pagerId)
-				return position;
-		return -1;
-	}
 
 	/**
 	 * 初始化各项配置。典型情况下在{@link #onCreate(Bundle)}调用
 	 */
 	protected void setup() {
-		setPagersOrder(Settings.getRole(this));
-		setupViewPager();
+		setMainMenuContent(Settings.getRole(this));
+		setupMainMenu();
 		setupActionBar();
 	}
 	/**
@@ -142,55 +125,27 @@ public class MainActivity extends BaseActivity {
 		ActionBar actionBar = getActionBar();
 		// Specify that the Home/Up button should not be enabled, since there is no hierarchical parent.
 		actionBar.setHomeButtonEnabled(false);
-		// Specify that tabs should be displayed in the action bar.
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		// Create a tab listener that is called when the user changes tabs.
-		ActionBar.TabListener tabListener = new ActionBar.TabListener() {
-			@Override
-			public void onTabSelected(Tab tab, FragmentTransaction ft) {
-				mViewPager.setCurrentItem(tab.getPosition(), true);
-			}
-			@Override
-			public void onTabUnselected(Tab tab, FragmentTransaction ft) {}
-			@Override
-			public void onTabReselected(Tab tab, FragmentTransaction ft) {}
-		};
-		// Add tabs, specifying the tab's text and TabListener
-		for(int i = 0 ;i < mPagersOrder.length ; i++)
-			actionBar.addTab(actionBar.newTab().setText(mPagersOrder[i]).setTabListener(tabListener));
 	}
 	/**
-	 * 配置{@link ViewPager}，典型情况下在{@link #onCreate(Bundle)}调用
+	 * 配置主菜单，典型情况下在{@link #onCreate(Bundle)}调用
 	 */
-	protected void setupViewPager() {
-		mViewPager.setAdapter(new AppSectionsPagerAdapter(getFragmentManager()));
-		mViewPager.setOnPageChangeListener(new OnPageChangeListener(){
-			@Override
-			public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
-			@Override
-			public void onPageSelected(int position) {
-				getActionBar().setSelectedNavigationItem(position);
-			}
-			@Override
-			public void onPageScrollStateChanged(int state) {}
-		});
+	protected void setupMainMenu() {
+		mMainMenuGridView.setAdapter(new MainMenuAdapter(this));
+		mMainMenuGridView.setOnItemClickListener(new OnMenuItemClickListener());
 	}
 
 	/**
-	 * 如果需要，改变Pagers的顺序。例如改变用户角色时
-	 * @return 如果Pagers的顺序改变了，返回true；如果无需改变，返回false
+	 * 如果需要，改变主菜单内容。例如改变用户角色时
+	 * @return 如果改变了，返回true；如果无需改变，返回false
 	 */
-	protected boolean changePagersOrderIfNecessary() {
+	protected boolean changeMainMenuIfNecessary() {
 		Role role = Settings.getRole(this);
 		if(mRole == role)
 			return false;
 		// 更新Pagers的顺序设置
-		setPagersOrder(role);
-		// 更新ActionBar的Tabs的顺序
-		for(int i = 0 ; i < mPagersOrder.length ; i++)
-			getActionBar().getTabAt(i).setText(mPagersOrder[i]);
+		setMainMenuContent(role);
 		// 通知ViewPager数据集有变化
-		mViewPager.getAdapter().notifyDataSetChanged();
+		((MainMenuAdapter) mMainMenuGridView.getAdapter()).notifyDataSetChanged();
 		return true;
 	}
 
@@ -255,69 +210,124 @@ public class MainActivity extends BaseActivity {
 	 * @see #getIntent()
 	 */
 	protected void handleIntent() {
-		Integer fragmentId = getIntentPager();
-		if(fragmentId != null) {
-			int position = getPagerPosition(fragmentId);
-			mViewPager.setCurrentItem(position, true);
+	}
+
+	private class MainMenuAdapter extends BaseAdapter {
+		private Context mContext;
+
+		public MainMenuAdapter(Context context) {
+			super();
+			mContext = context;
+		}
+		@Override
+		public int getCount() {
+			return mFunctions.length;
+		}
+		@Override
+		public Object getItem(int position) {
+			return mFunctions[position];
+		}
+		@Override
+		public long getItemId(int position) {
+			return mFunctions[position].ordinal();
+		}
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			TextView item = null;
+			if (convertView == null) {  // if it's not recycled, initialize some attributes
+				item = new TextView(mContext);
+				item.setGravity(Gravity.CENTER);
+				item.setTextAppearance(mContext, android.R.style.TextAppearance_Large);
+			} else {
+				item = (TextView) convertView;
+			}
+			Function function = mFunctions[position];
+			item.setText(function.getTitleResourceId());
+			item.setCompoundDrawablesWithIntrinsicBounds(0, function.getIconResourceId(), 0, 0);
+			return item;
+		}
+	}
+
+	private static class OnMenuItemClickListener implements OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			Function function = Function.values()[(int)id];
+			switch (function) {
+			case LOCATE_SERVICE:
+				break;
+			case FALL_DOWN_ALARM_SERVICE:
+				break;
+			case SEEK_HELP:
+				break;
+			case GIVE_CARER_A_CALL:
+				break;
+			case OUTBOX:
+				break;
+			case RESPONSE_MESSAGE:
+				break;
+			case CONTACTS_SETTING:
+				break;
+			case ROLE_SETTING:
+				break;
+
+			default:
+				throw new IllegalStateException("unsupport function: " + function.name());
+			}
 		}
 	}
 
 	/**
-	 * {@link MainActivity}中{@link ViewPager}的{@link PagerAdapter}，
-	 * 为{@link MainActivity}提供内容{@link Fragment}。
-	 * @see FragmentPagerAdapter
+	 * 本应用的功能。在主功能菜单中作为菜单项使用。
 	 * @author Team Orange
 	 */
-	protected class AppSectionsPagerAdapter extends FragmentPagerAdapter {
-		public AppSectionsPagerAdapter(FragmentManager fm) {
-			super(fm);
+	private static enum Function {
+		/** 定位服务（开关） */
+		LOCATE_SERVICE(R.string.locate_service, R.drawable.ic_main_menu_sample),
+		/** 摔倒检测服务（开关） */
+		FALL_DOWN_ALARM_SERVICE(R.string.fall_down_alarm_service, R.drawable.ic_main_menu_sample),
+		/** 求助 */
+		SEEK_HELP(R.string.seek_help, R.drawable.ic_main_menu_sample),
+		/** 给照料着打电话 */
+		GIVE_CARER_A_CALL(R.string.call, R.drawable.ic_main_menu_sample),
+		/** 发件箱（已发消息） */
+		OUTBOX(R.string.outbox, R.drawable.ic_main_menu_sample),
+		/** 回馈信息（相应消息） */
+		RESPONSE_MESSAGE(R.string.response_message, R.drawable.ic_main_menu_sample),
+		/** 联系人设置 */
+		CONTACTS_SETTING(R.string.contacts_setting, R.drawable.ic_main_menu_sample),
+		/** 角色设置 */
+		ROLE_SETTING(R.string.role_setting, R.drawable.ic_main_menu_sample);
+
+		private final int mIconResourceId;
+		private final int mTitleResourceId;
+
+		/**
+		 * @param titleResId 标题 {@link string}资源ID
+		 * @param iconResId 图标  {@link drawable}资源ID
+		 */
+		private Function(int titleResId, int iconResId) {
+			mIconResourceId = iconResId;
+			mTitleResourceId = titleResId;
 		}
-
-		@Override
-		public Fragment getItem(int position) {
-			Integer intentPager = getIntentPager();
-			Bundle args = buildFragmentArgumentsByIntent();
-			switch(mPagersOrder[position]){
-			case R.string.seek_help:
-				return new SeekHelpFragment();
-			case R.string.log:
-				Fragment logFragment = new LogFragment();
-				if(intentPager != null && intentPager == R.string.log)
-					logFragment.setArguments(args);
-				return logFragment;
-			case R.string.navigate:
-				return new NavigateFragment();
-			}
-			throw new IllegalArgumentException("illegal position: " + position);
+		public int getIconResourceId() {
+			return mIconResourceId;
 		}
-
-		@Override
-		public long getItemId(int position) {
-			return mPagersOrder[position];
-		}
-
-		@Override
-		public int getItemPosition(Object object) {
-			int id = -1;
-			if(object instanceof SeekHelpFragment)
-				id = R.string.seek_help;
-			else if(object instanceof LogFragment)
-				id = R.string.log;
-			else if(object instanceof NavigateFragment)
-				id = R.string.navigate;
-			else
-				throw new IllegalStateException("encounter unknown item");
-
-			int position = getPagerPosition(id);
-			if(position >= 0)
-				return position;
-			else
-				throw new IllegalStateException("Unknown pager or position");
-		}
-
-		@Override
-		public int getCount() {
-			return mPagersOrder.length;
+		public int getTitleResourceId() {
+			return mTitleResourceId;
 		}
 	}
+
+	private static final Function[] FUNCTIONS_GENERAL =
+	{Function.OUTBOX, Function.RESPONSE_MESSAGE, Function.CONTACTS_SETTING,
+	Function.ROLE_SETTING};
+	/** 受顾者的功能及其顺序 */
+	private static final Function[] FUNCTIONS_CAREE = ArrayUtils.addAll(
+			new Function[]{Function.LOCATE_SERVICE, Function.FALL_DOWN_ALARM_SERVICE,
+			Function.SEEK_HELP, Function.GIVE_CARER_A_CALL},
+			FUNCTIONS_GENERAL);
+	/** 照料者的功能及其顺序 */
+	private static final Function[] FUNCTIONS_CARER = ArrayUtils.addAll(
+			new Function[]{},
+			FUNCTIONS_GENERAL);
 }
