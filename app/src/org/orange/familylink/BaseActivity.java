2015ca9222228225445a2f8baa16f66823e95656
Bai-Jie <BaiJie1991@gmail.com>
2013-09-13 15:08:17 +0800

    隐藏设置Activity

--- app/src/org/orange/familylink/BaseActivity.java
+++ app/src/org/orange/familylink/BaseActivity.java
@@ -4,30 +4,9 @@
 package org.orange.familylink;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.view.Menu;
-import android.view.MenuItem;
 
 /**
  * @author Team Orange
  */
 public abstract class BaseActivity extends Activity {
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch(item.getItemId()) {
-		case R.id.action_settings:
-			startActivity(new Intent(this, SettingsActivity.class));
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
-
 }
