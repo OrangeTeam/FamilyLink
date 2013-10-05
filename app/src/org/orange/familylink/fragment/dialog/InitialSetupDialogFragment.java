3b824e144f7622d8b774889265830c67542a43fd
Bai-Jie <BaiJie1991@gmail.com>
2013-10-05 18:49:19 +0800

    改善MainActivity和初始化对话框的友好性，更易理解
    修改 换用一些更常用易懂的名词或提示
    修改 初始化对话框不允许用户输入空联系人姓名或电话

--- app/src/org/orange/familylink/fragment/dialog/InitialSetupDialogFragment.java
+++ app/src/org/orange/familylink/fragment/dialog/InitialSetupDialogFragment.java
@@ -14,14 +14,19 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.ContentResolver;
 import android.content.ContentValues;
-import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 /**
  * 初始化设置对话框。
@@ -30,30 +35,93 @@ import android.widget.Spinner;
 public class InitialSetupDialogFragment extends DialogFragment {
 	private OnClickListener mListener;
 
+	private Animation mAnimationShake;
+	private EditText mEditTextContactName;
+	private EditText mEditTextContactAddress;
+	private Spinner mSpinnerRole;
+
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mAnimationShake = AnimationUtils.loadAnimation(getActivity(), R.anim.shake);
+	}
+
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
 		setCancelable(false);
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 		// inflate dialog view
-		View dialogView = getActivity().getLayoutInflater()
+		final View dialogView = getActivity().getLayoutInflater()
 				.inflate(R.layout.dialog_fragment_initial_setup_dialog_view, null);
-		final EditText contactName = (EditText) dialogView.findViewById(R.id.contact_name_input);
-		final EditText contactAddress = (EditText) dialogView.findViewById(R.id.contact_address_input);
-		final Spinner roleInput = (Spinner) dialogView.findViewById(R.id.role);
+		mEditTextContactName = (EditText) dialogView.findViewById(R.id.contact_name_input);
+		mEditTextContactAddress = (EditText) dialogView.findViewById(R.id.contact_address_input);
+		mSpinnerRole = (Spinner) dialogView.findViewById(R.id.role);
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
 				getActivity(), R.array.pref_role_titles, android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		roleInput.setAdapter(adapter);
+		mSpinnerRole.setAdapter(adapter);
+		mSpinnerRole.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			private final String[] mRoles = getResources().getStringArray(R.array.pref_role_titles);
+			private final TextView mContactNameLable = (TextView) dialogView.findViewById(R.id.contact_name_title);
+			private final TextView mContactAddressLable = (TextView) dialogView.findViewById(R.id.contact_address_title);
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+				String currentRole;
+				if(position == 1)
+					currentRole = mRoles[0];
+				else if(position == 0)
+					currentRole = mRoles[1];
+				else
+					throw new UnsupportedOperationException("Unsupported roles selected:"+position);
+				mContactNameLable.setText(getString(R.string.initial_setup_dialog_contact_name_label, currentRole));
+				mContactAddressLable.setText(getString(R.string.initial_setup_dialog_contact_phone_label, currentRole));
+			}
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {}
+		});
 		// 默认选择“受顾者”，照顾用户心理，避免用户自己选择“受顾者”
-		roleInput.setSelection(1);
+		mSpinnerRole.setSelection(1);
 
 		builder.setView(dialogView)
 			.setTitle(R.string.initial_Setup)
-			.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+			.setPositiveButton(android.R.string.ok, null);
+		Dialog dialog = builder.create();
+		// 自动打开软键盘
+		dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+		return dialog;
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		//super.onStart() is where dialog.show() is actually called on the underlying dialog,
+		//so we have to do it after this point
+		AlertDialog dialog = (AlertDialog) getDialog();
+		if(dialog != null) {
+			Button positiveButton = dialog.getButton(Dialog.BUTTON_POSITIVE);
+			positiveButton.setOnClickListener(new View.OnClickListener() {
 				@Override
-				public void onClick(DialogInterface dialog, int which) {
+				public void onClick(View v) {
+					boolean inputValid = true;
+					// 保存联系人信息
+					String name = mEditTextContactName.getText().toString();
+					String address = mEditTextContactAddress.getText().toString();
+					if(TextUtils.isEmpty(address)) {
+						mEditTextContactAddress.requestFocus();
+						mEditTextContactAddress.startAnimation(mAnimationShake);
+						inputValid = false;
+					}
+					if(TextUtils.isEmpty(name)) {
+						mEditTextContactName.requestFocus();
+						mEditTextContactName.startAnimation(mAnimationShake);
+						inputValid = false;
+					}
+					if(!inputValid)
+						return;
+					new ContactUpdater(getActivity().getContentResolver()).execute(name, address);
 					// 保存角色信息
-					int position = roleInput.getSelectedItemPosition();
+					int position = mSpinnerRole.getSelectedItemPosition();
 					Role role = null;
 					switch(position){
 					case 0:
@@ -66,19 +134,13 @@ public class InitialSetupDialogFragment extends DialogFragment {
 						throw new IllegalStateException("illegal position :"+position);
 					}
 					Settings.setRole(getActivity(), role);
-					// 保存联系人信息
-					new ContactUpdater(getActivity().getContentResolver()).execute(
-							contactName.getText().toString(),
-							contactAddress.getText().toString());
 					// 通知监视器
 					if(mListener != null)
 						mListener.onClickPositiveButton(InitialSetupDialogFragment.this);
+					dismiss();
 				}
 			});
-		Dialog dialog = builder.create();
-		// 自动打开软键盘
-		dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
-		return dialog;
+		}
 	}
 
 	/**
